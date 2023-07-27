459e8399d126117ac5528d2e77ea78d0d1f90c6f
Hrithik <slumdog4321@gmail.com>
2023-07-27 11:12:33 +0530

    Sidebar added

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,18 @@
 import moment from "moment/moment";
 import css from "./Layout.module.css";
 import { BiSearch } from "react-icons/bi";
+import Sidebar from "../Sidebar/Sidebar";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const Layout = () => {
+  const { pathname } = useLocation();
   return (
     <div className={css.container}>
-      <div>Sidebar</div>
+      <Sidebar />
+
+      {/**make the defaluit route to dash board */}
+      {pathname === "/" && <Navigate to="/dashboard" />}
+
       <div className={css.dashboard}>
         <div className={css.topBaseGradients}>
           <div className="gradient-red"></div>
@@ -27,6 +34,9 @@ const Layout = () => {
               <span> hrithiknotforyou@gmail.com</span>
             </div>
           </div>
+        </div>
+        <div className={css.content}>
+          <Outlet />
         </div>
       </div>
     </div>
