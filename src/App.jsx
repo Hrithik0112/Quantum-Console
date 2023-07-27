04a244f5f12e82dbe52ad4c0d089420dc8fa3f6c
Hrithik <slumdog4321@gmail.com>
2023-07-27 17:51:31 +0530

    Trello board page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Layout from "../src/components/Layout/Layout";
 import Calender from "./pages/Calender/Calender";
+import BoardPage from "./pages/Board/Board";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
           <Route path="/" element={<Layout />}>
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="calender" element={<Calender />} />
+            <Route path="board" element={<BoardPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
