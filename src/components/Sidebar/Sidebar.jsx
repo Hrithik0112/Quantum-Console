f31d5e53b321dda038044412f719c4d5773c493d
Hrithik <slumdog4321@gmail.com>
2024-01-31 21:31:26 +0530

    remove moduler css from sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import css from "./Sidebar.module.css";
+import "./Sidebar.css";
 import { MdSpaceDashboard } from "react-icons/md";
 import { AiFillCalendar, AiOutlineTable } from "react-icons/ai";
 import { FaTasks } from "react-icons/fa";
@@ -6,19 +6,19 @@ import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className={css.conatiner}>
-      <img src="./logo.png" alt="logo" className={css.logo} />
-      <div className={css.menu}>
-        <NavLink to="dashboard" className={css.item} title="Dashboard">
+    <div className="container">
+      <img src="./logo.png" alt="logo" className="logo" />
+      <div className="menu">
+        <NavLink to="/dashboard" className="item" title="Dashboard">
           <MdSpaceDashboard size={30} />
         </NavLink>
-        <NavLink to="calender" className={css.item} title="Calender">
+        <NavLink to="/calender" className="item" title="Calender">
           <AiFillCalendar size={30} />
         </NavLink>
-        <NavLink to="board" className={css.item} title="Trello Board">
+        <NavLink to="/board" className="item" title="Trello Board">
           <FaTasks size={30} />
         </NavLink>
-        <NavLink to="users" className={css.item} title="Users">
+        <NavLink to="/users" className="item" title="Users">
           <AiOutlineTable size={30} />
         </NavLink>
       </div>
